ef6b5bf2af12600dfbf8e1bab04dd6512cad17c6
joaocarlosms <jcmeseverino@gmail.com>
2024-12-31 16:34:06 -0300

    feat: first commit

---
+++ src/main/java/pos_java_jdbc/pos_java_jdbc/App.java
@@ -0,0 +1,10 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
