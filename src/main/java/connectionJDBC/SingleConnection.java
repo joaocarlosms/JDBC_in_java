942f71d00b1b9f92f9ee691e872520fc89ed23a1
joaocarlosms <jcmeseverino@gmail.com>
2025-01-01 11:22:49 -0300

    feat: add class Student into model and add class StudentDao into dao

--- src/main/java/connectionJDBC/SingleConnection.java
+++ src/main/java/connectionJDBC/SingleConnection.java
@@ -9,10 +9,20 @@ public class SingleConnection {
 	private static String user = "myuser";
 	private static Connection connection = null;
 	
+	/*Utilizar o static dessa forma, automatiza nossa conexão
+	 * com o database toda vez que a classe seja instanciada
+	 * dessa forma, garantimos que a primeira coisa ser instanciada
+	 * na classe seja esse metodo static*/
 	static {
 		connect();
 	}
 	
+	/*Utilizamos nosso metodo connect no constructor para
+	 * caso a conexão com o nosso databsae não seja estabelecida,
+	 * ao utilizar o construtor, ele estabelece a conexão com o db.
+	 * Mesmo que ele consiga estabelecer a conexão no nosso metodo static
+	 * ele ainda passa pelo constructor só para garantir que está
+	 * tudo de acordos*/
 	public SingleConnection() {
 		connect();
 	}
