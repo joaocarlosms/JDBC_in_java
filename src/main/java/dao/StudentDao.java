bdcb4f022f0183835f7a29e9289bb99d46bb2749
joaocarlosms <jcmeseverino@gmail.com>
2025-01-01 12:39:24 -0300

    feat: add method to saveStudent into database

--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -1,13 +1,39 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import connectionJDBC.SingleConnection;
+import model.Student;
 
 public class StudentDao {
 	private Connection connection;
 	
 	public StudentDao() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void saveStudent(Student student) {
+		try {
+			
+			String sql = "INSERT INTO student (id, name_st, email) VALUES (?, ?, ?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, student.getId());
+			insert.setString(2, student.getNameSt());
+			insert.setString(3, student.getEmail());
+			insert.execute(); //executa o comand sql
+			connection.commit();
+			
+		} catch(Exception e) {
+			
+			try {
+				connection.rollback(); //reverte operações no db
+			} catch(SQLException ex) {
+				ex.printStackTrace();
+			}
+			
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/pos_java_jdbc/pos_java_jdbc/TestDatabaseJDBC.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TestDatabaseJDBC.java
@@ -3,11 +3,16 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.jupiter.api.Test;
 
 import connectionJDBC.SingleConnection;
+import dao.StudentDao;
+import model.Student;
 
 public class TestDatabaseJDBC {
 	
 	@Test
 	public void initDatabase() {
-		SingleConnection.getConnection();
+		StudentDao studentDao = new StudentDao();
+		Student st = new Student(2L, "João Carlos", "jc@gmail.com");
+		
+		studentDao.saveStudent(st);
 	}
 }
