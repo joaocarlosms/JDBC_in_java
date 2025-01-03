2f2c2343c628a5674530d85646297738341428a1
joaocarlosms <jcmeseverino@gmail.com>
2025-01-03 20:46:39 -0300

    feat: add class Phone

---
+++ src/main/java/model/Phone.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Phone {
+	private Long id;
+	private String number_phone;
+	private String type_phone;
+	private Student student_phone;
+	
+	public Phone() {
+		this.id = null;
+		this.number_phone = "";
+		this.type_phone = "";
+		this.student_phone = null;
+	}
+	
+	public Phone(Long id, String number_phone, String type_phone, Student student_phone) {
+		this.id = id;
+		this.number_phone = number_phone;
+		this.type_phone = type_phone;
+		this.student_phone = student_phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumber_phone() {
+		return number_phone;
+	}
+
+	public void setNumber_phone(String number_phone) {
+		this.number_phone = number_phone;
+	}
+
+	public String getType_phone() {
+		return type_phone;
+	}
+
+	public void setType_phone(String type_phone) {
+		this.type_phone = type_phone;
+	}
+
+	public Student getStudent_phone() {
+		return student_phone;
+	}
+
+	public void setStudent_phone(Student student_phone) {
+		this.student_phone = student_phone;
+	}
+}
